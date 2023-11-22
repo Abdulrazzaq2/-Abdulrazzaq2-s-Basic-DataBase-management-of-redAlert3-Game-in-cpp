96c745884a7562f61fc6cb4771a0efd3a3cd10fe
Abdulrazzaq2 <96846834+Abdulrazzaq2@users.noreply.github.com>
2023-11-23 00:43:09 +0300

    add database.sql
    easy for importing into XAMPP APP

---
+++ abdulrazaqDBproject/abdrazaq1.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 22, 2023 at 10:41 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `abdrazaq1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `redalert3`
+--
+
+CREATE TABLE `redalert3` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `redalert3`
+--
+
+INSERT INTO `redalert3` (`id`, `name`, `country`) VALUES
+(1, 'George', 'Japan'),
+(2, 'Lee', 'UK'),
+(3, 'mohammed', 'Egypt'),
+(4, 'Abdulrazzaq', 'Iraq'),
+(5, 'Mark', 'Austria'),
+(6, 'Ali', 'Iraq');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `redalert3`
+--
+ALTER TABLE `redalert3`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
